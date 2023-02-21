d81d0a5cf3732f8c6cce3296e52f682aaeb4cd79
Michael Andrew Rodriguez Martinez <michaelandrewrm@gmail.com>
2023-02-21 21:49:54 +0100

    add some eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,4 +17,40 @@ module.exports = {
 	parserOptions: {
 		ecmaVersion: 'latest',
 	},
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-await-in-loop': 'error',
+		'no-constant-binary-expression': 'error',
+		'no-constructor-return': 'error',
+		'no-duplicate-imports': 'error',
+		'no-new-native-nonconstructor': 'error',
+		'no-promise-executor-return': 'error',
+		'no-self-compare': 'error',
+		'no-use-before-define': [
+			'error',
+			{
+				functions: true,
+				classes: true,
+				variables: true,
+				allowNamedExports: false,
+			},
+		],
+		'arrow-body-style': ['error', 'as-needed'],
+		'camelcase': [
+			'error',
+			{
+				properties: 'always',
+				ignoreDestructuring: false,
+				ignoreImports: false,
+				ignoreGlobals: false,
+			},
+		],
+		'default-case': 'off',
+		'dot-notation': 'warn',
+		'eqeqeq': 'warn',
+		'no-alert': 'error',
+		'no-eq-null': 'error',
+		'rest-spread-spacing': ['error', 'never'],
+	},
 };
